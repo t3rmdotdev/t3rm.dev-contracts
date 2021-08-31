e426b8ba989b9aae4bf45baef69ebaca4f50fe2d
t3rmdotdev <89753866+t3rmdotdev@users.noreply.github.com>
2021-08-30 20:13:41 -0700

    Final contract
    Deployed to mainnet

--- t3rm.sol
+++ t3rm.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: MIT
+// https://t3rm.dev
 pragma solidity ^0.8.0;
 
 import "./ERC721URIStorage.sol";
@@ -22,12 +23,12 @@ contract t3rm is ERC721URIStorage, Ownable {
    uint private _launchBlock;
 
   /**
-   * Mapping of command hash to tokenID.
+   * Mapping of package hash to tokenID.
    */
-   mapping(bytes32 => uint) private _commands;
+   mapping(bytes32 => uint) private _packages;
 
    /**
-   * Mapping of tokenID to command hash.
+   * Mapping of tokenID to package hash.
    */
    mapping(uint => string) private _tokens;
 
@@ -37,7 +38,7 @@ contract t3rm is ERC721URIStorage, Ownable {
    mapping(uint => address) private _creators;
 
   /**
-   * Mapping of command to frozen status.
+   * Mapping of package to frozen status.
    */
   mapping(uint => bool) private _frozen;
 
@@ -49,7 +50,7 @@ contract t3rm is ERC721URIStorage, Ownable {
   /**
    * Mint fee
    *
-   * Defines the price required to register a command.
+   * Defines the price required to register a package.
    */
   uint private _mintFee;
 
@@ -114,34 +115,34 @@ contract t3rm is ERC721URIStorage, Ownable {
     _launchBlock = __launchBlock;
 
     // Reserved: connect
-    _commands[0x06e7a5e6cac387364da652310717d5f91789fd895ebf5dc658e0dcd80a2f9a42] = type(uint256).max;
+    _packages[0x06e7a5e6cac387364da652310717d5f91789fd895ebf5dc658e0dcd80a2f9a42] = type(uint256).max;
 
     // Reserved: disconnect
-    _commands[0x017399084a6301db582204dd3505f7ead52eb83de2b3c608d8503256263026cf] = type(uint256).max;
+    _packages[0x017399084a6301db582204dd3505f7ead52eb83de2b3c608d8503256263026cf] = type(uint256).max;
 
     // Reserved: info
-    _commands[0x3820cab827512459062f4d6dd584c03d5f2ec29517c1c68c886f18f041fc4fc7] = type(uint256).max;
+    _packages[0x3820cab827512459062f4d6dd584c03d5f2ec29517c1c68c886f18f041fc4fc7] = type(uint256).max;
 
     // Reserved: list
-    _commands[0xbe8e25a9981f15070e47df9c1c85829f53ccec91a1340eac095113ab184fae5f] = type(uint256).max;
+    _packages[0xbe8e25a9981f15070e47df9c1c85829f53ccec91a1340eac095113ab184fae5f] = type(uint256).max;
 
     // Reserved: mint
-    _commands[0x5b4ea791576315f49763e091eb2b21ee4f1789045afc8e036b44288714996993] = type(uint256).max;
+    _packages[0x5b4ea791576315f49763e091eb2b21ee4f1789045afc8e036b44288714996993] = type(uint256).max;
 
     // Reserved: update
-    _commands[0xfdcff868f2b2010d7dde03b445b78c840f2a012dd6208aa14b5c673094f0aa31] = type(uint256).max;
+    _packages[0xfdcff868f2b2010d7dde03b445b78c840f2a012dd6208aa14b5c673094f0aa31] = type(uint256).max;
 
     // Reserved: freeze
-    _commands[0x1238908b973638e0ad25d4933d8d3a76f918aaa8cbb3bee6bc1d0adcb26cec59] = type(uint256).max;
+    _packages[0x1238908b973638e0ad25d4933d8d3a76f918aaa8cbb3bee6bc1d0adcb26cec59] = type(uint256).max;
   }
 
   /**
-   * Command Hash helper.
+   * package Hash helper.
    *
    * Accepts lowercase letters, numbers, hyphens, periods, and underscores.
    * Returns a keccak256 hash.
    */
-  function _commandHash(string memory str) private pure returns (bytes32) {
+  function _packageHash(string memory str) private pure returns (bytes32) {
     bytes memory b = bytes(str);
 
     for (uint i; i<b.length; i++){
@@ -153,7 +154,7 @@ contract t3rm is ERC721URIStorage, Ownable {
         (char == 0x2D) || //-
         (char == 0x2E) || //.
         (char == 0x5F) //_
-      , "Command contains invalid characters.");
+      , "package contains invalid characters.");
     }
 
     return keccak256(abi.encode(string(b)));
@@ -197,24 +198,24 @@ contract t3rm is ERC721URIStorage, Ownable {
   }
 
   /**
-   * Get token ID of command.
+   * Get token ID of package.
    *
    * Returns the token ID.
    */
-  function token(string memory _command) public view returns (uint) {
-    bytes32 _cmd = _commandHash(_command);
-    uint tokedID = _commands[_cmd];
-    require(tokedID > 0, "Command not found.");
+  function token(string memory _package) public view returns (uint) {
+    bytes32 _cmd = _packageHash(_package);
+    uint tokedID = _packages[_cmd];
+    require(tokedID > 0, "package not found.");
 
     return tokedID;
   }
 
   /**
-   * Get the command string for a tokenID.
+   * Get the package string for a tokenID.
    *
-   * Returns the registered command.
+   * Returns the registered package.
    */
-  function command(uint _tokenId) public view returns (string memory) {
+  function package(uint _tokenId) public view returns (string memory) {
     require(_tokenId <= _tokenIDs.current(), "Token doesn't exist.");
 
     return _tokens[_tokenId];
@@ -236,10 +237,10 @@ contract t3rm is ERC721URIStorage, Ownable {
    *
    * Returns true if a tokenURI is unable to be updated.
    */
-  function frozen(string memory _command) public view returns (bool) {
-    bytes32 _cmd = _commandHash(_command);
-    uint tokedID = _commands[_cmd];
-    require(tokedID > 0, "Command not found.");
+  function frozen(string memory _package) public view returns (bool) {
+    bytes32 _cmd = _packageHash(_package);
+    uint tokedID = _packages[_cmd];
+    require(tokedID > 0, "package not found.");
 
     return _frozen[tokedID];
   }
@@ -266,12 +267,12 @@ contract t3rm is ERC721URIStorage, Ownable {
   /**
    * Get code
    *
-   * Returns the source code multihash for a command.
+   * Returns the source code multihash for a package.
    */
-  function code(string memory _command) public view returns (string memory) {
-    bytes32 _cmd = _commandHash(_command);
-    uint tokenID = _commands[_cmd];
-    require(tokenID > 0, "Command not found.");
+  function code(string memory _package) public view returns (string memory) {
+    bytes32 _cmd = _packageHash(_package);
+    uint tokenID = _packages[_cmd];
+    require(tokenID > 0, "package not found.");
 
     return tokenURI(tokenID);
   }
@@ -298,14 +299,14 @@ contract t3rm is ERC721URIStorage, Ownable {
    */
   function mintTo(
     address _receiver,
-    string memory _command,
+    string memory _package,
     string memory _tokenURI
   ) public payable returns (uint) {
     require(block.number >= _launchBlock, "Platform hasn't launched.");
     require(msg.value >= _mintFee, "Requires minimum fee.");
 
-    bytes32 _cmd = _commandHash(_command);
-    require(_commands[_cmd] == 0, "Command in use.");
+    bytes32 _cmd = _packageHash(_package);
+    require(_packages[_cmd] == 0, "package in use.");
 
     payable(owner()).transfer(msg.value);
 
@@ -316,8 +317,8 @@ contract t3rm is ERC721URIStorage, Ownable {
     uint tokenId = _tokenIDs.current();
     _mint(_receiver, tokenId);
     _setTokenURI(tokenId, _tokenURI);
-    _commands[_cmd] = tokenId;
-    _tokens[tokenId] = _command;
+    _packages[_cmd] = tokenId;
+    _tokens[tokenId] = _package;
     _creators[tokenId] = msg.sender;
 
     return tokenId;
@@ -328,19 +329,19 @@ contract t3rm is ERC721URIStorage, Ownable {
    *
    * Requires payment of _mintFee.
    */
-  function mint(string memory _command, string memory _tokenURI) public payable returns (uint) {
-    return mintTo(msg.sender, _command, _tokenURI);
+  function mint(string memory _package, string memory _tokenURI) public payable returns (uint) {
+    return mintTo(msg.sender, _package, _tokenURI);
   }
 
   /**
-   * Update a command.
+   * Update a package.
    *
    * Requires ownership of token.
    */
-  function update(string memory _command, string memory _tokenURI) public {
-    bytes32 _cmd = _commandHash(_command);
-    uint tokenID = _commands[_cmd];
-    require(tokenID > 0, "Command not found.");
+  function update(string memory _package, string memory _tokenURI) public {
+    bytes32 _cmd = _packageHash(_package);
+    uint tokenID = _packages[_cmd];
+    require(tokenID > 0, "package not found.");
     require(ownerOf(tokenID) == msg.sender, "Only the owner can update the token.");
     require(!_frozen[tokenID], "Token is frozen and cannot be updated.");
 
@@ -348,14 +349,14 @@ contract t3rm is ERC721URIStorage, Ownable {
   }
 
   /**
-   * Freeze a command.
+   * Freeze a package.
    *
    * Requires ownership of token.
    */
-  function freeze(string memory _command) public {
-    bytes32 _cmd = _commandHash(_command);
-    uint tokenID = _commands[_cmd];
-    require(tokenID > 0, "Command not found.");
+  function freeze(string memory _package) public {
+    bytes32 _cmd = _packageHash(_package);
+    uint tokenID = _packages[_cmd];
+    require(tokenID > 0, "package not found.");
     require(ownerOf(tokenID) == msg.sender, "Only the owner can freeze the token.");
     require(!_frozen[tokenID], "Already frozen.");
 
